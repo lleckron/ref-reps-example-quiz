a90ef8a7e620a603b874bf317bdcd4f1a08c9bfd
lleckron <leckmanbaseball@gmail.com>
2023-04-18 19:43:01 -0400

    setup new vue project

---
+++ web/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="container">
+		<p>Test</p>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'App',
+	components: {
+		
+	},
+	data() {
+		return {
+			
+		}
+	},
+	methods: {
+		
+	}
+}
+</script>
+
+<style>
+
+
+</style>
---
+++ web/src/components/NavbarHeader.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+	name: 'NavbarHeader',
+	components: {
+		
+	},
+	data() {
+		return {
+			
+		}
+	},
+	methods: {
+		
+	}
+}
+</script>
+
+<style>
+
+
+</style>
